114b1f36181e4ef221e2ba93732cb43b29210a23
Julius <68971573+jdcbautista@users.noreply.github.com>
2023-11-18 15:51:02 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Welcome to PHP test page</h1>
+<a href = "https://media.giphy.com/media/UuebWyG4pts3rboawU/giphy.gif/"Target="_blank">Hello world!</a>
+<pre>
+<?php
+#print ("https://media.giphy.com/media/UuebWyG4pts3rboawU/giphy.gif/ ");
+?>
+</pre>
+</body>
+</html>
